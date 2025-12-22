6e3bc8710741121874ebecc854d9d941cd38c5bc
Satish Singh <i.sksingh113@gmail.com>
2025-12-23 01:53:14 +0530

    fix

--- services/clearTokenCookieAstrologer.js
+++ services/clearTokenCookieAstrologer.js
@@ -3,18 +3,18 @@ const { serialize } = require("cookie");
 const clearTokenCookieAstrologer = (res) => {
   res.setHeader("Set-Cookie", [
     serialize("token", "", {
-  //  domain:".graho.in",
+   domain:".graho.in",
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       expires: new Date(0),
     }),
     serialize("token_astrologer", "", {
-//  domain:".graho.in",
+ domain:".graho.in",
       httpOnly: false,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       path: "/",
       expires: new Date(0),
     }),
